18e4f54312eabbbd1f7a21460b2c8f7dc1d6631d
Andrew Godwin <andrew@aeracode.org>
2019-12-02 23:32:33 -0700

    Started Django app to act as central storage

---
+++ listkeeper/directory/admin.py
@@ -0,0 +1,19 @@
+from django.contrib import admin
+
+from .models import Tag, Item, Label, Location, Appearance
+
+admin.site.register(Tag)
+admin.site.register(Label)
+admin.site.register(Location)
+admin.site.register(Appearance)
+
+
+@admin.register(Item)
+class ItemAdmin(admin.ModelAdmin):
+    fieldsets = [
+        (None, {"fields": ["name", "serial", "notes", "location", "labels"]}),
+        ("Timestamps", {"fields": ["created", "updated", "deleted"]}),
+    ]
+    list_display = ["id", "name", "serial", "location", "created", "updated"]
+    list_display_links = ["id", "name"]
+    readonly_fields = ["created", "updated"]
---
+++ listkeeper/directory/models.py
@@ -0,0 +1,118 @@
+import uuid
+
+from django.db import models
+
+
+class Tag(models.Model):
+    """
+    A reference to a single physical tag.
+
+    We allow for multiple tags per physical item.
+    """
+
+    id = models.CharField(
+        max_length=255,
+        primary_key=True,
+        help_text="Type prefix, colon, hex value of tag (e.g. epc:f376ce13434a2b)",
+    )
+    item = models.ForeignKey(
+        "directory.Item",
+        blank=True,
+        null=True,
+        related_name="tags",
+        on_delete=models.PROTECT,
+    )
+
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+    deleted = models.DateTimeField(blank=True, null=True)
+
+    def __str__(self):
+        return self.id
+
+
+class Item(models.Model):
+    """
+    A physical item, with zero or more tags on it.
+    """
+
+    name = models.CharField(max_length=255)
+    serial = models.TextField(blank=True)
+    notes = models.TextField(blank=True)
+
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+    deleted = models.DateTimeField(blank=True, null=True)
+
+    location = models.ForeignKey(
+        "directory.Location",
+        related_name="items",
+        blank=True,
+        null=True,
+        on_delete=models.PROTECT,
+    )
+    labels = models.ManyToManyField("directory.Label", related_name="items", blank=True)
+
+    def __str__(self):
+        return "%s: %s" % (self.id, self.name)
+
+
+class Label(models.Model):
+    """
+    An arbitary categorisation of Items, many allowed. Anywhere else, would be
+    called "tag", but that's not a great word to re-use here.
+    """
+
+    name = models.CharField(max_length=100)
+
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.name
+
+
+class Location(models.Model):
+    """
+    A location that objects can be in.
+
+    Locations may be nested inside other locations.
+    """
+
+    name = models.CharField(max_length=255)
+    parent = models.ForeignKey(
+        "self", blank=True, null=True, related_name="children", on_delete=models.PROTECT
+    )
+
+    latitude = models.FloatField(blank=True, null=True)
+    longitude = models.FloatField(blank=True, null=True)
+    elevation = models.FloatField(blank=True, null=True)
+
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+    deleted = models.DateTimeField(blank=True, null=True)
+
+    def __str__(self):
+        return self.name
+
+
+class Appearance(models.Model):
+    """
+    A log of when an item was seen in a location.
+    """
+
+    item = models.ForeignKey(
+        "directory.Item", related_name="appearances", on_delete=models.PROTECT
+    )
+    location = models.ForeignKey(
+        "directory.Location", related_name="appearances", on_delete=models.PROTECT
+    )
+    timestamp = models.DateTimeField()
+
+    def __str__(self):
+        return "%s: %s in %s at %s" % (
+            self.id,
+            self.item,
+            self.location,
+            self.timestamp,
+        )
