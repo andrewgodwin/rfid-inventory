6e90fc8a8958f20ff53bb1e78c5e83e4fd4069b6
Andrew Godwin <andrew@aeracode.org>
2019-12-09 20:41:47 -0700

    Finish off basic deploy script

--- client-chafon/deploy.py
+++ client-chafon/deploy.py
@@ -6,9 +6,18 @@ from invoke import UnexpectedExit
 
 
 @click.command()
+@click.option("--device", default="/dev/ttyUSB0")
 @click.argument("host")
-def main(host):
+@click.argument("url")
+@click.argument("token")
+def main(host, url, token, device):
     connection = Connection(host)
+    # Stop the service if it's running and installed
+    click.echo(click.style("Stopping any existing installation...", fg="green", bold=True))
+    try:
+        connection.sudo("systemctl stop rfid-client")
+    except UnexpectedExit:
+        pass
     # Ensure base tools are installed
     click.echo(click.style("Installing base tools...", fg="green", bold=True))
     connection.sudo("apt-get update -q")
@@ -25,7 +34,30 @@ def main(host):
     # Install packages
     click.echo(click.style("Installing Python packages...", fg="green", bold=True))
     connection.sudo("pip3 install -r /srv/rfid-inventory/client-chafon/requirements.txt")
+    # Write out a systemd unit file
+    click.echo(click.style("Writing systemd unit file...", fg="green", bold=True))
+    unit_file = unit_template.format(device=device, url=url, token=token).strip()
+    connection.sudo("bash -c \"echo '%s' > /etc/systemd/system/rfid-client.service\"" % unit_file)
+    # Start it
+    click.echo(click.style("Enabling and starting service...", fg="green", bold=True))
+    connection.sudo("systemctl daemon-reload")
+    connection.sudo("systemctl enable rfid-client")
+    connection.sudo("systemctl start rfid-client")
 
+
+unit_template = """
+[Unit]
+Description=RFID scanner client
+
+[Service]
+Type=simple
+Restart=always
+RestartSec=5
+ExecStart=/usr/bin/python3 /srv/rfid-inventory/client-chafon/client.py {device} {url} {token}
+
+[Install]
+WantedBy=multi-user.target
+"""
 
 if __name__ == "__main__":
     main()
