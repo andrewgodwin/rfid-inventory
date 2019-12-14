9949af00e56986f1718f24a1e194d6191e5d3178
Andrew Godwin <andrew@aeracode.org>
2019-12-14 11:33:24 -0700

    Update a few deployment pieces

---
+++ listkeeper/listkeeper/production_settings.py
@@ -0,0 +1,13 @@
+import os
+
+DATABASES = {
+    "default": {
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": os.environ.get("PGDATABASE", "listkeeper"),
+        "HOST": os.environ.get("PGHOST", ""),
+        "USER": os.environ.get("PGUSER", "postgres"),
+        "PASSWORD": os.environ.get("PGPASSWORD", "postgres"),
+    }
+}
+
+ALLOWED_HOSTS = [x.strip() for x in os.environ["ALLOWED_HOSTS"].split(",") if x.strip()]
