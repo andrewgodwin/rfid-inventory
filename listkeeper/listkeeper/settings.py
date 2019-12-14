c3415a4c2e10b1d1a6998f509d413f1d32768d37
Andrew Godwin <andrew@aeracode.org>
2019-12-14 12:01:11 -0700

    Try to get static files working in prod

--- listkeeper/listkeeper/settings.py
+++ listkeeper/listkeeper/settings.py
@@ -119,6 +119,8 @@ LOGOUT_REDIRECT_URL = "/"
 
 STATIC_URL = "/static/"
 
+STATIC_ROOT = os.path.join(BASE_DIR, "static-collected")
+
 STATICFILES_DIRS = [os.path.join(BASE_DIR, "static")]
 
 STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
