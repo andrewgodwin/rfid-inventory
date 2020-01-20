fd38212f4f45f86ed4485da785a55983d8cf9746
Andrew Godwin <andrew@aeracode.org>
2020-01-20 10:29:04 -0700

    Have screen always on

--- zebra-android/app/src/main/java/org/aeracode/inventory/clients/androidclient/MainActivity.java
+++ zebra-android/app/src/main/java/org/aeracode/inventory/clients/androidclient/MainActivity.java
@@ -9,6 +9,7 @@ import android.support.v7.widget.Toolbar;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.WindowManager;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 import android.widget.TextView;
